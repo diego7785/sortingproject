ff0a822a4019c50edc9687476380819b507bb592
Diego Andrés Bonilla Viáfara <diego.viafara@correounivalle.edu.co>
2021-11-25 14:08:50 -0500

    adding cors to backend

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from .algorithms import algorithms
+from flask_cors import CORS
+
 def create_app():
 	app = Flask(__name__)
 
+	CORS(app)
 	# Blueprints register
 	app.register_blueprint(algorithms)
 
